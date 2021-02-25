2d1597c6f5542e4502f535ab81803fa388b8cee9
zachlim98 <zachlim98@users.noreply.github.com>
2021-02-25 15:34:56 +0800

    Final upload of daily telegram message

--- Account Tracking/tdaAPI2/config.py
+++
@@ -1,4 +0,0 @@
-CONSUMER_KEY = "DRSMU4TL964FO3QNBQHVL78X9SUPGGIL"
-REDIRECT_URL = "http://localhost"
-JSON_PATH = "X:/GitHub/beatingENd/BeatingEnDUS/Account Tracking/R_tracking/secure/token.json"
-TD_ACCOUNT = "220535376"
