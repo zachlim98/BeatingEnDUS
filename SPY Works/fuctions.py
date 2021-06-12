081b8b61c75cb3e2139833376466610a26693685
zachlim98 <krismerful@gmail.com>
2021-06-12 12:03:14 +0800

    Added Account Manipulation Ops

--- SPY Works/fuctions.py
+++ SPY Works/fuctions.py
@@ -1,6 +1,6 @@
 class Trade:
     def __init__(self, id, value, exp):
-        self.id = id 
+        self.id = id
         self.value = value
         self.exp = exp
 
@@ -14,9 +14,20 @@ class AccountBal:
     def __init__(self, value):
         self.value = value
         self.trade_val = []
+        self.account_bal = []
 
     def add_trade(self, trade_val):
         self.trade_val.append(trade_val)
 
     def update_balance(self, change):
         self.value = self.value + change
+        self.account_bal.append(self.value)
+
+class TradeOperations:
+    @staticmethod
+    def close_trade(account, curr_trade, close_val, next_trade):
+        account.add_trade(curr_trade.get_value())
+        account.update_balance(curr_trade.get_value() - close_val)
+        curr_trade.update_trade(close_val)
+        account.add_trade(-curr_trade.get_value())
+        account.add_trade(next_trade.get_value())
--- SPY Works/testing.py
+++ SPY Works/testing.py
@@ -1,15 +1,15 @@
-from fuctions import Trade, AccountBal
+from fuctions import Trade, AccountBal, TradeOperations
 from datetime import timedelta
 from datetime import date
 import pandas as pd
-import glob
 
 # truncated = pd.read_pickle("truncated.pickle")
 
-curr_trade = Trade("SPY2019-06-10P288", 3.40, date(2019,6,10))
-next_trade = Trade("SPY2019-06-17P290", 2.40, date(2019,6,17))
+curr_trade = Trade("SPY2019-06-10P288", 3.40, date(2019, 6, 10))
 my_account = AccountBal(100)
+next_trade = Trade("SPY2019-06-17P290", 2.40, date(2019, 6, 17))
+TradeOperations.close_trade(my_account, curr_trade, 0.40, next_trade)
+curr_trade = next_trade
 
-my_account.add_trade(curr_trade.get_value())
 print(my_account.trade_val)
-
+print(my_account.account_bal)
